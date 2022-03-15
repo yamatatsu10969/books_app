5465e1aa73af07ca344e14556b648af1c7709420
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-03-15 22:42:09 +0900

    author をカラムに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_13_114617) do
+ActiveRecord::Schema.define(version: 2022_03_15_133631) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
